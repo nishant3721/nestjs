3847f6226617909c0b46a19b04e8e2dbfc25d996
Nishant Singh <srnikhil3721@gmail.com>
2021-12-20 00:42:40 +0530

    car restful apis done

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { CarService } from './car.service';
+import { CarDTO } from './car.dto';
 
 @Controller('car')
 export class CarController {
   constructor(private carService: CarService) {}
 
-  @Get('getCars')
-  async getCars() {
+  @Get()
+  public getCars() {
     return this.carService.getCars();
+  }
+
+  @Get(':id')
+  public async getCarById(@Param('id') id: number) {
+    return this.carService.getCarById(id);
+  }
+
+  @Post()
+  public addCar(@Body() car: CarDTO) {
+    return this.carService.addCar(car);
+  }
+
+  @Delete(':id')
+  public async deleteCarById(@Param('id') id: number) {
+    return this.carService.deleteCarById(id);
+  }
+
+  @Put(':id')
+  public async updateCarById(@Param('id') id: number, @Query() query) {
+    const propertyName = query.property_name;
+    const propertyValue = query.property_value;
+    return this.carService.updateCarById(id, propertyName, propertyValue);
   }
 }
--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,11 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { CARS } from './cars.mock';
 
 @Injectable()
 export class CarService {
   private cars = CARS;
 
-  public async getCars() {
+  public getCars() {
     return this.cars;
+  }
+
+  public getCarById(id: number): Promise<any> {
+    const carId = Number(id);
+    return new Promise((resolve) => {
+      const car = this.cars.find((item) => item.id == carId);
+      if (!car) {
+        throw new HttpException(
+          'Car with the given id is not found in our database',
+          404,
+        );
+      }
+      return resolve(car);
+    });
+  }
+
+  public addCar(car) {
+    return this.cars.push(car);
+    // return 'Car added';
+  }
+
+  public deleteCarById(id: number): Promise<any> {
+    const carId = Number(id);
+    return new Promise((resolve) => {
+      const index = this.cars.findIndex((item) => item.id == carId);
+      if (index === -1) {
+        throw new HttpException(
+          'Car with the given id is not found in our database',
+          404,
+        );
+      }
+      this.cars.splice(index, 1);
+      return resolve(this.cars);
+    });
+  }
+
+  public updateCarById(
+    id: number,
+    propertyName: string,
+    propertyValue: string,
+  ): Promise<any> {
+    const carId = Number(id);
+    return new Promise((resolve) => {
+      const index = this.cars.findIndex((item) => item.id == carId);
+      if (index === -1) {
+        throw new HttpException(
+          'Car with the given id is not found in our database',
+          404,
+        );
+      }
+      this.cars[index][propertyName] = propertyValue;
+      return resolve(this.cars);
+    });
   }
 }
